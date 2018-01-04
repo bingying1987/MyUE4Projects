f3d415df6d281b9f2b211029ca3cab0c2875c1ae
Mr.Wu <12259145@163.com>
2018-01-05 02:16:16 +0800

    0.0.3

--- Escape/Source/Escape/OpenDoor.cpp
+++ Escape/Source/Escape/OpenDoor.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReporter.h"
+#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
-UPositionReporter::UPositionReporter()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -12,25 +12,30 @@ UPositionReporter::UPositionReporter()
 
 	// ...
 }
-
+void UOpenDoor::OpenDoor()
+{
+	GetOwner()->SetActorRotation(FRotator(0.0f, 60.0f, 0.0f));
+}
 
 // Called when the game starts
-void UPositionReporter::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 	// ...
-	FString ObjectName = GetOwner()->GetName();
-	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"),*ObjectName,*ObjectPos);
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 }
 
 
 // Called every frame
-void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+	}
 }
 
---
+++ Escape/Source/Escape/OpenDoor.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	void OpenDoor();
+private:
+	UPROPERTY(VisibleAnyWhere)
+		float OpenAngle = 60.0f;
+	UPROPERTY(EditAnyWhere)
+		ATriggerVolume *PressurePlate;
+	
+	AActor *ActorThatOpens;
+};
